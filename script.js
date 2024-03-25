296074dace9f802fddab48aa53a3ff77c3346d66
Nguyen Son Tung <tungns.wd@gmail.com>
2024-03-25 13:13:23 +0700

    Implementing holding the current score and checking the winner

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 'use strict';
+
 document.addEventListener('contextmenu', e => e.preventDefault());
-
-const dice = document.querySelector('.dice');
-const rollBtn = document.querySelector('.btn-roll');
-const holdBtn = document.querySelector('.btn-hold');
-const resetBtn = document.querySelector('.btn-reset');
-const player0 = document.querySelector('.player-0');
-const player1 = document.querySelector('.player-1');
-
-const displayText = function (selector, message) {
-  return document.getElementById(selector).textContent = message;
-}
 
 const random = function () { return Math.trunc(Math.random() * 6) + 1 };
 
 // Generating a rolled dice random
 const randomDice = function () {
-  rollDice(random());
+  return rollDice(random());
+}
+
+const queryNode = function (node) {
+  return document.querySelector(node);
 }
 
 let activePlayer = 0;
 let scoresArchive = 0;
+let scoresSelector = `#scoresHold-${activePlayer}`;
+let archiveSelector = `#scoresArchive-${activePlayer}`;
+
+const scores = [0, 0];
+const dice = queryNode('.dice');
+const rollBtn = queryNode('.btn-roll');
+const holdBtn = queryNode('.btn-hold');
+const resetBtn = queryNode('.btn-reset');
+const player0 = queryNode('.player-0');
+const player1 = queryNode('.player-1');
+
+const toggleClass = function (node, className) {
+  node.classList.toggle(className);
+}
+
+const displayText = function (node, message) {
+  return queryNode(node).textContent = message;
+}
+
+const insertAfter = function (refNode, newNode) {
+  return refNode.parentNode.insertBefore(newNode, refNode.nextSibling);
+}
+
+const switchPlayer = function () {
+  scoresArchive = 0;
+  displayText(archiveSelector, scoresArchive);
+  toggleClass(player0, '_active');
+  toggleClass(player1, '_active');
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  scoresSelector = `#scoresHold-${activePlayer}`;
+  archiveSelector = `#scoresArchive-${activePlayer}`;
+}
 
 // Rolling dice functionality
 const rollDice = function (rolledDice) {
@@ -61,27 +87,40 @@ const rollDice = function (rolledDice) {
     dice.style.animation = 'none';
 
     // Check for rolled 1
-    let activeSelector = `scoresArchive-${activePlayer}`;
-
     if (rolledDice !== 1) {
       scoresArchive += rolledDice;
-      displayText(activeSelector, scoresArchive);
+      displayText(archiveSelector, scoresArchive);
       // Switches player
-    } else {
-      scoresArchive = 0;
-      displayText(activeSelector, scoresArchive);
-      player0.classList.toggle('_active');
-      player1.classList.toggle('_active');
-      activePlayer = activePlayer === 0 ? 1 : 0;
-    }
+    } else { switchPlayer() }
 
-    rollBtn.classList.toggle('disabled');
+    toggleClass(rollBtn, 'disabled');
+    toggleClass(holdBtn, 'disabled');
 
   }, 1550);
 
 }
 
 rollBtn.addEventListener('click', function () {
-  rollBtn.classList.toggle('disabled');
+  toggleClass(rollBtn, 'disabled');
+  toggleClass(holdBtn, 'disabled');
   randomDice();
+});
+
+holdBtn.addEventListener('click', function () {
+  scores[activePlayer] += scoresArchive;
+  displayText(scoresSelector, scores[activePlayer]);
+  if (scores[activePlayer] >= 100) {
+    scoresArchive = 0;
+    displayText(archiveSelector, scoresArchive);
+    toggleClass(rollBtn, 'disabled');
+    toggleClass(holdBtn, 'disabled');
+    const trophyNode = document.createElement('p');
+    trophyNode.classList.add('score');
+    trophyNode.innerHTML = '🏆';
+    const scoresNode = queryNode(scoresSelector);
+    insertAfter(scoresNode, trophyNode);
+    const winnerNode = queryNode(`.player-${activePlayer}`);
+    toggleClass(winnerNode, '_winner');
+    dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  } else { switchPlayer(); }
 });
